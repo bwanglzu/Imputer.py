2ec59923ccff36e777f3f416e1d6479837cbf787
wb <519369865@qq.com>
2017-04-19 19:28:02 +0200

    add ci, requirements, pep8 adaption

--- imputer/imputer.py
+++ imputer/imputer.py
@@ -3,118 +3,114 @@ import numpy as np
 import pandas as pd
 from sklearn import neighbors
 
+
 class Imputer:
-	def fit(self, X, column, k=10, is_categorical = False):
-		"""Fit a knn classifier for missing column
-		Args:
-			X(numpy.ndarray): input data
-			column(int): column id to be imputed
-			k(int): number of nearest neighbors, default 10
-			is_categorical(boolean): is continuous or categorical feature
-		Returns:
-			clf: trained k nearest neighbour classifier
-		"""
-		clf = None
-		if not is_categorical:
-			clf = neighbors.KNeighborsRegressor(n_neighbors=k)
-		else:
-			clf = neighbors.KNeighborsClassifier(n_neighbors=k)
-		#use column not null to train the kNN classifier
-		missing_idxes = np.where(pd.isnull(X[:,column]))[0]
-		if len(missing_idxes) == 0:
-			sys.exit()
-		X_copy = np.delete(X, missing_idxes, 0)
-		X_train = np.delete(X_copy, column, 1)
-		#if other columns still have missing values fill with mean
-		col_mean = None
-		if not is_categorical:
-			col_mean = np.nanmean(X, 0)
-		else:
-			col_mean = np.nanmedian(X, 0)
-		for col_id in range(0,len(col_mean)-1):
-			col_missing_idxes = np.where(np.isnan(X_train[:,col_id]))[0]
-			if len(col_missing_idxes)==0:
-				continue
-			else:
-				X_train[col_missing_idxes,col_id]=col_mean[col_id]
-		y_train = X_copy[:,column]
-		#fit classifier
-		clf.fit(X_train, y_train)
-		return clf
 
-	def transform(self, X, column, clf, is_categorical):
-		"""Impute missing values
-		Args:
-			X(numpy.ndarray): input numpy ndarray
-			column(int): index of column to be imputed
-			clf: pretrained classifier
-			is_categorical(boolean): is continuous or categorical feature
-		Returns:
-			X(pandas.dataframe): imputed dataframe
-		"""
-		missing_idxes = np.where(np.isnan(X[:,column]))[0]
-		X_test = X[missing_idxes,:]
-		X_test = np.delete(X_test, column, 1)
-		#if other columns still have missing values fill with mean
-		col_mean = None
-		if not is_categorical:
-			col_mean = np.nanmean(X, 0)
-		else:
-			col_mean = np.nanmedian(X,0)
-		#fill missing values in each column with current col_mean
-		for col_id in range(0,len(col_mean)-1):
-			col_missing_idxes = np.where(np.isnan(X_test[:,col_id]))[0]
-			#if no missing values for current column
-			if len(col_missing_idxes)==0:
-				continue
-			else:
-				X_test[col_missing_idxes,col_id]=col_mean[col_id]
-		#predict missing values
-		y_test = clf.predict(X_test)
-		X[missing_idxes,column]=y_test
-		return X
+    def fit(self, X, column, k=10, is_categorical=False):
+        """Fit a knn classifier for missing column.
 
-	def knn(self, X, column, k=10, is_categorical = False):
-		"""Impute missing value with knn
-		Args:
-			X(pandas.dataframe): dataframe
-			column(str): column name to be imputed
-			k(int): number of nearest neighbors, default 10
-			is_categorical(boolean): is continuous or categorical feature
-		Returns:
-			X_imputed(pandas.dataframe): imputed pandas dataframe
-		"""
-		X, column = self.check_X_y(X, column)
-		clf = self.fit(X, column, k, is_categorical)
-		X_imputed = self.transform(X, column, clf, is_categorical)
-		return X_imputed
+        - Args:
+                X(numpy.ndarray): input data
+                column(int): column id to be imputed
+                k(int): number of nearest neighbors, default 10
+                is_categorical(boolean): is continuous or categorical feature
+        - Returns:
+                clf: trained k nearest neighbour classifier
+        """
+        clf = None
+        if not is_categorical:
+            clf = neighbors.KNeighborsRegressor(n_neighbors=k)
+        else:
+            clf = neighbors.KNeighborsClassifier(n_neighbors=k)
+        # use column not null to train the kNN classifier
+        missing_idxes = np.where(pd.isnull(X[:, column]))[0]
+        if len(missing_idxes) == 0:
+            sys.exit()
+        X_copy = np.delete(X, missing_idxes, 0)
+        X_train = np.delete(X_copy, column, 1)
+        # if other columns still have missing values fill with mean
+        col_mean = None
+        if not is_categorical:
+            col_mean = np.nanmean(X, 0)
+        else:
+            col_mean = np.nanmedian(X, 0)
+        for col_id in range(0, len(col_mean) - 1):
+            col_missing_idxes = np.where(np.isnan(X_train[:, col_id]))[0]
+            if len(col_missing_idxes) == 0:
+                continue
+            else:
+                X_train[col_missing_idxes, col_id] = col_mean[col_id]
+        y_train = X_copy[:, column]
+        # fit classifier
+        clf.fit(X_train, y_train)
+        return clf
 
-	def check_X_y(self, X, column):
-		"""Check input, if pandas.dataframe, transform to numpy array
-		Args:
-			X(ndarray/pandas.dataframe): input instances
-			column(str/int): column index or column name
-		Returns:
-			X(ndarray): input instances
-		"""
-		column_idx = None
-		if isinstance(X, pd.core.frame.DataFrame):
-			if isinstance(column, str):
-				#get index of current column
-				column_idx = X.columns.get_loc(column)
-			else:
-				column_idx = column
-			X = X.as_matrix()
-		else:
-			column_idx = column
-		return X,column_idx
+    def transform(self, X, column, clf, is_categorical):
+        """Impute missing values.
 
+        - Args:
+                X(numpy.ndarray): input numpy ndarray
+                column(int): index of column to be imputed
+                clf: pretrained classifier
+                is_categorical(boolean): is continuous or categorical feature
+        - Returns:
+                X(pandas.dataframe): imputed dataframe
+        """
+        missing_idxes = np.where(np.isnan(X[:, column]))[0]
+        X_test = X[missing_idxes, :]
+        X_test = np.delete(X_test, column, 1)
+        # if other columns still have missing values fill with mean
+        col_mean = None
+        if not is_categorical:
+            col_mean = np.nanmean(X, 0)
+        else:
+            col_mean = np.nanmedian(X, 0)
+        # fill missing values in each column with current col_mean
+        for col_id in range(0, len(col_mean) - 1):
+            col_missing_idxes = np.where(np.isnan(X_test[:, col_id]))[0]
+            # if no missing values for current column
+            if len(col_missing_idxes) == 0:
+                continue
+            else:
+                X_test[col_missing_idxes, col_id] = col_mean[col_id]
+        # predict missing values
+        y_test = clf.predict(X_test)
+        X[missing_idxes, column] = y_test
+        return X
 
+    def knn(self, X, column, k=10, is_categorical=False):
+        """Impute missing value with knn.
 
+        - Args:
+                X(pandas.dataframe): dataframe
+                column(str): column name to be imputed
+                k(int): number of nearest neighbors, default 10
+                is_categorical(boolean): is continuous or categorical feature
+        - Returns:
+                X_imputed(pandas.dataframe): imputed pandas dataframe
+        """
+        X, column = self.check_X_y(X, column)
+        clf = self.fit(X, column, k, is_categorical)
+        X_imputed = self.transform(X, column, clf, is_categorical)
+        return X_imputed
 
+    def check_X_y(self, X, column):
+        """Check input, if pandas.dataframe, transform to numpy array.
 
-
-
-
-
-
+        - Args:
+                X(ndarray/pandas.dataframe): input instances
+                column(str/int): column index or column name
+        - Returns:
+                X(ndarray): input instances
+        """
+        column_idx = None
+        if isinstance(X, pd.core.frame.DataFrame):
+            if isinstance(column, str):
+                # get index of current column
+                column_idx = X.columns.get_loc(column)
+            else:
+                column_idx = column
+            X = X.as_matrix()
+        else:
+            column_idx = column
+        return X, column_idx
