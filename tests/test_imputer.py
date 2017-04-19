ab9c1d75248e69656129013765d94fc8c027d9cc
wb <519369865@qq.com>
2017-04-19 19:34:48 +0200

    add test folder

---
+++ tests/test_imputer.py
@@ -0,0 +1,12 @@
+import unittest
+
+import sys
+sys.path.append('..')
+from imputer.imputer import Imputer()
+
+class TestImputer(unittest.TestCase):
+	def test_imputer(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
