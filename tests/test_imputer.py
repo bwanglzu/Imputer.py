4da9c234eb34cb018d68ecfe964249f2f8a0382a
wb <519369865@qq.com>
2017-04-23 19:49:33 +0200

    remove example, add test

--- tests/test_imputer.py
+++ tests/test_imputer.py
@@ -1,7 +1,8 @@
 """Test imputer."""
+import os
 import unittest
-import sys
-sys.path.append('..')
+import numpy as np
+import pandas as pd
 from imputer.imputer import Imputer
 
 
@@ -10,9 +11,15 @@ class TestImputer(unittest.TestCase):
 
     def test_imputer(self):
         """Test imputer function."""
+        path = os.path.dirname(__file__) + "/train"
         impute = Imputer()
-        type(impute)
-        pass
+        X = pd.read_csv(path)
+        impute_first_column = impute.knn(X, 0)
+        impute_eighth_column = impute.knn(X, 8, k=10, is_categorical=True)
+        self.assertIsNotNone(impute_first_column)
+        self.assertIsNotNone(impute_eighth_column)
+        self.assertIsInstance(impute_first_column, np.ndarray)
+        self.assertIsInstance(impute_eighth_column, np.ndarray)
 
 
 if __name__ == '__main__':
