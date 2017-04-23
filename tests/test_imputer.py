98d44824736dd3c7be54581f3e333a15e84daf0c
wb <519369865@qq.com>
2017-04-23 20:06:31 +0200

    update test

--- tests/test_imputer.py
+++ tests/test_imputer.py
@@ -13,13 +13,31 @@ class TestImputer(unittest.TestCase):
         """Test imputer function."""
         path = os.path.dirname(__file__) + "/train"
         impute = Imputer()
-        X = pd.read_csv(path)
+        cols = ['age', 'work_class', 'education',
+                'num_edu_years', 'marital_status', 'occupation',
+                'relationship', 'race', 'sex',
+                'income', 'loss', 'hour',
+                'country', 'label']
+        X = pd.read_csv(path, names=cols, header=None)
         impute_first_column = impute.knn(X, 0)
         impute_eighth_column = impute.knn(X, 8, k=10, is_categorical=True)
         self.assertIsNotNone(impute_first_column)
         self.assertIsNotNone(impute_eighth_column)
         self.assertIsInstance(impute_first_column, np.ndarray)
         self.assertIsInstance(impute_eighth_column, np.ndarray)
+        # column has no missing values
+        impute_first_column = impute.knn(X, 0)
+        self.assertIsNotNone(impute_first_column)
+        self.assertIsInstance(impute_first_column, np.ndarray)
+        # user specify a column name
+        impute_second_column = impute.knn(X, 'work_class')
+        self.assertIsNotNone(impute_second_column)
+        self.assertIsInstance(impute_second_column, np.ndarray)
+        # user input is a ndarray
+        X = np.genfromtxt(path, delimiter=',')
+        impute_third_column = impute.knn(X, 2)
+        self.assertIsNotNone(impute_third_column)
+        self.assertIsInstance(impute_third_column, np.ndarray)
 
 
 if __name__ == '__main__':
